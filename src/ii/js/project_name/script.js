a59ddaafca44d178739fdeb3cc0e2ddb27529cc2
Evgeniya Dikova <minwuori@mail.ru>
2019-04-13 22:27:01 +0300

    clouds animate

--- src/ii/js/project_name/script.js
+++ src/ii/js/project_name/script.js
@@ -18,69 +18,69 @@ document.addEventListener('DOMContentLoaded', function(){ // Аналог $(docu
 //   }
 // })();
 
-// var parallax = (function() {
-//   var screen = document.querySelector('.greeting');
-//   var svg = screen.querySelector('.bg');
+var parallax = (function() {
+  var screen = document.querySelector('.greeting');
+  var svg = screen.querySelector('.scene');
 
-//   var $els = Array.prototype.slice.call(svg.querySelectorAll('[data-parallax]'));
+  var $three = Array.prototype.slice.call(svg.querySelectorAll('[data-parallax]'));
 
-//   var start, position, dir, diff, 
-//   limit = 10, 
-//   speed = 0.0003,
-//   factor = 1.0001;
+  var start, position, dir, diff, 
+  limit = 10, 
+  speed = 0.0003,
+  factor = 1.0001;
 
-//   var move = function(e) {
-//     if (!start && start !== 0) {
-//       // еще движений не было
-//       // старт с позиции курсора
-//       start = position = e.clientX;
-//       diff = 0; // исходный сдвиг
-//       return;
-//     }
-//     // направление движения
-//     var ratio = e.clientX - position > 0 ? 1 : -1;
+  var move = function(e) {
+    if (!start && start !== 0) {
+      // еще движений не было
+      // старт с позиции курсора
+      start = position = e.clientX;
+      diff = 0; // исходный сдвиг
+      return;
+    }
+    // направление движения
+    var ratio = e.clientX - position > 0 ? 1 : -1;
     
-//     // если еще не было движения
-//     // устанавливаем исходное направление
-//     if (!dir) dir = ratio;
+    // если еще не было движения
+    // устанавливаем исходное направление
+    if (!dir) dir = ratio;
 
-//     if (ratio !== dir) { // начал двигаться в другом направлении
-//       // сбрасываем стартовую позицию
-//       start = position;
-//       // меняем направление
-//       dir = ratio;
-//       // сдвигаем в другую сторону
-//     } 
+    if (ratio !== dir) { // начал двигаться в другом направлении
+      // сбрасываем стартовую позицию
+      start = position;
+      // меняем направление
+      dir = ratio;
+      // сдвигаем в другую сторону
+    } 
 
-//     // устанавливаем новую текущую позицию
-//     position = e.clientX;
+    // устанавливаем новую текущую позицию
+    position = e.clientX;
 
-//     // расстояние сдвига курсора
-//     var dist = e.clientX - start;
-//     // рассчитываем сдвиг фона
-//     // сдвиг курсора * скорость движения
-//     var newDiff = dist * speed;
-//     diff = diff + newDiff;
-//     // проверяем пределы сдвига
-//     if (Math.abs(diff) > limit) diff = diff > 0 ? limit : -1 * limit;
+    // расстояние сдвига курсора
+    var dist = e.clientX - start;
+    // рассчитываем сдвиг фона
+    // сдвиг курсора * скорость движения
+    var newDiff = dist * speed;
+    diff = diff + newDiff;
+    // проверяем пределы сдвига
+    if (Math.abs(diff) > limit) diff = diff > 0 ? limit : -1 * limit;
 
-//     // сдвигаем все слои
-//     $els.forEach(function(el) {
-//       el.style.transform = 'translateX(' + (el.dataset.parallax * factor * diff) + 'px)';
-//     })
-//   };
+    // сдвигаем все слои
+    $three.forEach(function(el) {
+      el.style.transform = 'translateX(' + (el.dataset.parallax * factor * diff) + 'px)';
+    })
+  };
 
-//   var reset = function() {
-//     start = null;
-//     position = null;
-//     $els.forEach(function(el) {
-//       el.style.transform = 'translateX(0px)';
-//     })
-//   };
+  var reset = function() {
+    start = null;
+    position = null;
+    $three.forEach(function(el) {
+      el.style.transform = 'translateX(0px)';
+    })
+  };
 
-//   screen.parentElement.addEventListener('mousemove', move)
-//   screen.parentElement.addEventListener('mouseleave', reset)
-// })();
+  screen.parentElement.addEventListener('mousemove', move)
+  screen.parentElement.addEventListener('mouseleave', reset)
+})();
 
 //анимация медведя
 
@@ -89,12 +89,15 @@ var bear = document.querySelector('.bear'),
     hat = bear.querySelector('.bear__hat'),
     hatLeft = bear.querySelector('.bear__left'),
     mouth = bear.querySelector('.bear__mouth'),
+    eyebrow = bear.querySelector('.bear__eyebrows'),
     eyebrowLeft = bear.querySelector('.bear__eyebrow1'),
     eyebrowRight = bear.querySelector('.bear__eyebrow2'),
     nose = bear.querySelector('.bear__nose'),
-    tlMain = new TimelineMax({});
+    ray = document.querySelector('.ray'),
+    clouds = document.querySelector('.clouds'),
+    bearAnimate = new TimelineMax({});
 
-
+function balalaikaAnimate(){
   var bezierBalalaika = new TweenMax(balalaika , 4, {
     bezier:{
       type:"soft", 
@@ -107,6 +110,9 @@ var bear = document.querySelector('.bear'),
     },
   //rotation: 45, 
   ease:Linear.easeInOut, repeat: -1});
+};
+balalaikaAnimate();
+
 
   // var clouds = new TweenMax('.clouds' , 10, {
   //   bezier:{
@@ -120,49 +126,65 @@ var bear = document.querySelector('.bear'),
   // //rotation: 45, 
   // ease:Linear.easeNone, repeat: -1});
 
-  var clouds = new TweenMax('.clouds' , 20, {
+  // var clouds = new TweenMax('.clouds' , 20, {
+  //   bezier:{
+  //     type:"soft", 
+  //     values:[{left: 100, top: 110},
+  //             {left: -3800, top: 350},
+  //             {left: 100, top: 110},
+  //             {left: 600, top: 130},
+  //             {left: 2000, top: 300}],
+  //   },
+  // opacity: 0.9, 
+  // ease:Linear.ease, repeat: -1});
+
+
+var cloudsAnimate = new TimelineMax({repeat: -1});
+cloudsAnimate.fromTo(clouds, 3, {y: 110 }, {y: 0, yoyo: true})
+  .to(clouds, 3, {opacity: 0.5})
+  .to(clouds, 1, {opacity: 1})
+  .to(clouds, 6, {x: -1200, y: 190, ease:Linear.easeOut}, "+=1")
+  .to(clouds, 15, {
     bezier:{
       type:"soft", 
-      values:[{left: 100, top: 110},
-              {left: -3800, top: 350},
-              {left: 100, top: 110},
-              {left: 600, top: 130},
-              {left: 2000, top: 300}],
+      values:[{x: -1200, y: 190},
+              {x: -200, y: -190},
+              {x: 190, y: 110},
+              {x: 1800, y: -110}],
     },
-  opacity: 0.9, 
-  ease:Linear.ease, repeat: -1});
-  
-
-bear.addEventListener('click', function(){
-  var noseAnimate = tlMain.to(nose, 0.2, {x: -5}).to(nose, 0.2, {x: 5}).to(nose, 0.2, {x: -5}).to(nose, 0.2, {x: 5}).to(nose, 0.2, {x: 0});
+    ease:Linear.easeIn,
+    opacity: 0.6
 });
+var rayAnimate = new TimelineMax({repeat: -1});
+rayAnimate.fromTo(ray, 3, {opacity: 1}, {opacity: 0.3}).to(ray, 3, {opacity: 1});
+
 
 bear.addEventListener('mouseenter', function(){
-  tlMain.to(balalaika, 1, { y: 30, x: 0})
+  bearAnimate.to(balalaika, 1, { y: 30, x: 0})
         .to(balalaika, 2, { y: -105, x: 30, rotation: -30, transformOrigin: 'center right' })
-        .to(balalaika, 1, { y: -115, x: 30, ease:Back.easeOut });
-  TweenMax.to(hat, 2, { y: -90, x: -60, delay: 1.5, rotation: -30, transformOrigin: 'center right', ease:Back.easeOut });
-  TweenMax.to(hatLeft, 2, { y: -10, x: -30, delay: 1.5, rotation: 60, transformOrigin: 'center right', ease:Bounce.easeOut });
+        .to(balalaika, 1, { y: -115, x: 30, ease:Back.easeOut })
+        .to(hat, 2, { y: -90, x: -60, rotation: -30, transformOrigin: 'center right', ease:Back.easeOut }, "-=2.5")
+        .to(hatLeft, 2, { y: -10, x: -30, rotation: 60, transformOrigin: 'center right', ease:Bounce.easeOut }, "-=1.5")
+        .to(mouth, 1, {  delay: 1.5,  y: -5, x: -5,  rotation: 10, transformOrigin: 'center center' }, "-=3")
+        .to(balalaika, 2, { y: 0, x: 0, rotation: 0, transformOrigin: 'center right' })
+        .to(hat, 2, { y: 0, x: 0, rotation: 0, transformOrigin: 'center right', ease:Back.easeOut },"-=1.5");
 
-  TweenMax.to(mouth, 1, {  delay: 1.5,  y: -5, x: -5,  rotation: 10, transformOrigin: 'center center' });
-
-  var bezierEyebrow = new TweenMax([eyebrowLeft, eyebrowRight] , 0.8, {
+  var eyebrowAnimate = new TweenMax([eyebrowLeft, eyebrowRight] , 0.8, {
     bezier:{
       type:"soft", 
       values:[{x:0, y:-20}, {x:0, y:0}],
       autoRotate:false
     },
-  ease:Linear.easeInOut, repeat:10});
-})
+  ease:Linear.easeInOut, repeat:-1});
+  bearAnimate.to(nose, 0.2, {delay: 0.8, x: -3}).to(nose, 0.2, {x: 3}).to(nose, 0.2, {x: -3}).to(nose, 0.2, {x: 3}).to(nose, 0.2, {x: 0});
+});
 
 bear.addEventListener('mouseleave', function(){
 
-
-  TweenMax.to([balalaika, hat, hatLeft], 1, { y: 0, x: 0, rotation: 0, transformOrigin: 'center right' });
-
   TweenMax.to(mouth, 4, { dalay: 0.5, y: 0, x: 0, rotation: 0 });
-
-})
+  balalaikaAnimate();
+  
+});
 
 
 });
